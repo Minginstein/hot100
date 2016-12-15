d358434f930ba98007640f89f439d12548f4b8f2
David Roberts <davidhroberts1@gmail.com>
2016-12-15 15:24:01 -0500

    added convolutional neural network

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-## Hot100: An Analysis of Billboard's Hot 100 since 1958
+# Hot100: An Analysis of Billboard's Hot 100 since 1958
 
-# Overview:
-This repository contains data collection scripts, sample data files, as well as a Jupyter notebook outlining the data analysis techniques.
+## Overview:
+This repository contains data collection scripts, sample data files, as well as Jupyter notebooks outlining data analysis techniques.
 
-# Key research questions:
+## Key research questions:
 * How have the audio features of the Hot 100 evolved over time?
  * Is it possible to map the emergence of popular genres via Spotify Audio Features? 
 * Which artists have achieved longevity on Spotify?
  *Is it possible to model an artist's longevity as a function of their choices in musical production? 
 
-# Learning goals:
+## Learning goals:
 * Work fluidly with web-scraping, gui automation and OCR technologies (Beutiful Soup, Sikuli, Tesseract, OpenCV)
 * Shallow dive into numpy and pandas.
 * Learn Jupyter presentation format as well as more visually appealing data visualization libraries.
-* Leverage Python machine learning tools for unique analyses (keras, sklearn)
+* Leverage Python machine learning tools for unique analyses (Keras, sklearn)
 
-# Data sources:
+## Data sources:
 * Billboard Hot 100 archives (http://www.billboard.com/archive/charts/1965/hot-100)
 * Spotify Web API (https://developer.spotify.com/web-api/)
 * Spotify Desktop Client
--- data_collection/nn_training.py
+++ data_collection/nn_training.py
@@ -10,7 +10,7 @@ NN Training Script
 # core libraries
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, Convolution2D, MaxPooling2D, Flatten
 from keras.utils import np_utils
 
 # I/O operations
@@ -35,14 +35,14 @@ def rescale_images(flattened_image_matrix, scaling_factor= .50):
     
     new_shape = (int(ORIGINAL_SIZE[0]*scaling_factor), int(ORIGINAL_SIZE[1]*scaling_factor))
     new_size = new_shape[0]*new_shape[1]
-    new_array = np.empty((0, new_size), dtype = np.uint8)
+    new_matrix = np.empty((0, new_size), dtype = np.uint8)
     
     for i in range(flattened_image_matrix.shape[0]):
         row = flattened_image_matrix[i].astype(np.uint8)
         image = row.reshape(ORIGINAL_SIZE)  # note this should be parameterized by a master variable script
         new_image = cv2.resize(image, new_shape[::-1])
         new_flattened_row = new_image.reshape((1, new_size))
-        new_matrix = np.append(new_array, new_flattened_row, axis = 0)
+        new_matrix = np.append(new_matrix, new_flattened_row, axis = 0)
         
     return new_matrix
       
@@ -58,13 +58,40 @@ def baseline_model(num_pixels, num_classes):
     # compile the model - train using Adam gradient descent method
     model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
     return model
-
+    
+def CNN_model_basic(num_classes, image_shape):
+    """
+    Constructs a 6 layer convolutional neural network with the following layers:
+        1. Convolution layer - 32 filters (feature maps) convolve over pixel map, generating similarity "scores" for each pixel.
+             - Results in a matrix of scores with the same size as the image
+        2. Pooling layer which essentially scales this similiarty scores matrix down to half the size
+        3. Dropout - regularization layer which randomly drops specified proporiton of neurons
+        4. Flatten - flattens arrays out to be processed by standard layers
+        5. Dense - standard, fully connected hidden layer
+        6. Dense - 10 class output layer
+    """
+    
+    # create model
+    model = Sequential()
+    model.add(Convolution2D(32, 5, 5, border_mode='valid', input_shape=(1, image_shape[0], image_shape[1]), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.1))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+	# Compile model
+    model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+    
+    return model
+    
 # initializing random seed to ensure reproducability
 np.random.seed(10)
 
+#%%
 # loading data, transforming to desirable format
 dataset = load_data()
 dataset['image'] = rescale_images(dataset['image'], scaling_factor = .3)
+IMAGE_SHAPE = (45, 28)
 
 #%%
 dataset['image'] = dataset['image'] / 255
@@ -80,8 +107,12 @@ y_val = dataset['label'][sep:]
 num_pixels = dataset['image'].shape[1]
 
 #%%
+"""
+Training a baseline model
+"""
+
 model = baseline_model(num_pixels, 10)
-model.fit(X_train, y_train, validation_data = (X_val, y_val), nb_epoch = 5, batch_size = 200, verbose = 2)
+model.fit(X_train, y_train, validation_data = (X_val, y_val), nb_epoch = 5, batch_size = 200, verbose = 2, image_dim_ordering = 'tf')
 
 # 8:37 start time, 8:41 finish\
 scores = model.evaluate(X_val, y_val, verbose=0)
@@ -107,19 +138,24 @@ Since we're looking simply at pixel values as opposed to hog features or contour
 The shrinking process essentially maps surrounding pixels to one value, boiling more complex patterns down to simpler contours.  
 """
 
-#==============================================================================
-# #%%
-# dataset_2 = load_data()
-# 
-# def show_digit(img, label):
-#     reshaped = img.reshape((150, 95))
-#     cv2.imshow("IMAGE LABEL" + str(label), reshaped)
-#     cv2.waitKey(0)
-#     
-# for i in range(15):
-#     img = dataset_2['image'][i].astype(np.uint8)
-#     label = dataset_2['label'][i]
-#     print(label)
-#     show_digit(img, label)
-#     
-#==============================================================================
+#%%
+"""
+Training a basic convolutional model
+"""
+
+X_train_CNN = X_train.reshape(X_train.shape[0], 1, IMAGE_SHAPE[0], IMAGE_SHAPE[1]).astype('float32')
+X_val_CNN = X_val.reshape(X_val.shape[0], 1, IMAGE_SHAPE[0], IMAGE_SHAPE[1]).astype('float32')
+
+CNN_basic = CNN_model_basic(10, IMAGE_SHAPE)
+CNN_basic.fit(X_train_CNN, y_train, validation_data = (X_val_CNN, y_val), nb_epoch = 5, batch_size = 200, verbose = 2)
+
+#%%
+scores = CNN_basic.evaluate(X_val_CNN, y_val, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+
+# Baseline Error: 0.19%
+
+"""
+Seems to me that the error could likely be chalked up to labeling errors on my part
+The regularization probably caused a decreases in accuracy over the basic model.
+"""
